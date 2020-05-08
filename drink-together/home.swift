3493699b1bf387272d124ad033a98bc9f827874a
Mark Baumann <57982395+marky-mark-88@users.noreply.github.com>
2020-05-08 20:38:50 +0200

    Views (register, login) hinzugefügt - noch nicht verknüpft an die Firebase Datenbank

---
+++ drink-together/home.swift
@@ -0,0 +1,30 @@
+//
+//  home.swift
+//  drink-together
+//
+//  Created by Mark Baumann on 08.05.20.
+//  Copyright © 2020 Mark Baumann. All rights reserved.
+//
+
+import UIKit
+
+class home: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
