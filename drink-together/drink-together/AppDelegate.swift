46bfc7a6f52bd05472159a5ae34105f354c679d8
Mark Baumann <57982395+marky-mark-88@users.noreply.github.com>
2020-05-08 19:40:28 +0200

    Firebase Libary

--- drink-together/drink-together/AppDelegate.swift
+++ drink-together/drink-together/AppDelegate.swift
@@ -7,14 +7,18 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication,  _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
         return true
     }
 
