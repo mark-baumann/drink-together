39d0c7ba402a5f28685f9403de02adfaa0060efc
Mark Baumann <60445235+mark-baumann@users.noreply.github.com>
2020-12-08 22:43:15 +0100

    Burger Menu finished

--- DrinkTogether/View Controllers/BurgerMenu.swift
+++ DrinkTogether/View Controllers/BurgerMenu.swift
@@ -2,71 +2,45 @@
 //  BurgerMenu.swift
 //  DrinkTogether
 //
-//  Created by Mark Baumann   on 04.12.20.
+//  Created by Mark Baumann   on 08.12.20.
 //
 
 import UIKit
-import SideMenu
 
 class BurgerMenu: UIViewController {
 
-    var menu:SideMenuNavigationController?
+    
+    @IBOutlet weak var leadingCon: NSLayoutConstraint!
+    
+    @IBOutlet weak var TrailingCon: NSLayoutConstraint!
+    
+    
+    var menuOut = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-          
-        
-        menu = SideMenuNavigationController(rootViewController: menuListController())
+
         // Do any additional setup after loading the view.
-        menu?.leftSide = true
-        
-        SideMenuManager.default.leftMenuNavigationController = menu
-        
-        SideMenuManager.default.addScreenEdgePanGesturesToPresent(toView: self.view)
-        
-        
-    }
-    
-    @IBAction func didTap() {
-        present(menu!, animated: true)
     }
     
 
-}
-
-
-class menuListController: UITableViewController {
-    
-    var items = ["first", "second", "third"]
-    
-    let darkColor = UIColor(red: 33/255.0, green: 33/255.0, blue: 33/255.0, alpha: 1)
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tableView.backgroundColor = darkColor
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = items[indexPath.row]
-        cell.textLabel?.textColor = .white
-        cell.backgroundColor = darkColor
-        return cell
-    }
-    
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
+    @IBAction func BurgerMenuTapped(_ sender: Any) {
         
+        
+        if (menuOut == false) {
+            leadingCon.constant = 150
+            TrailingCon.constant = -150
+            menuOut = true
+        }else {
+            leadingCon.constant = 0
+            TrailingCon.constant = 0
+            menuOut = false
+        }
         
         
     }
     
-
     
+    
+
 }
